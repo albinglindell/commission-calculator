628e83f515ef9ebcb37c599445c327514fcd6ebe
Albin Lindell <albinglindell98@gmail.com>
2023-06-15 22:18:14 +0200

    fixed the size on ol

--- src/components/FlightList.jsx
+++ src/components/FlightList.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function FlightList({flight}) {
   return (
     <div className='flightList'>
-        <h2>Name: {flight.flightNr}</h2>
-        <h3>Commission: {flight.provision}</h3>
+        <h2 className='flightName'>Name: {flight.flightNr}</h2>
+        <h3 className='provision'>Commission: {flight.provision}</h3>
     </div>
   )
 }
